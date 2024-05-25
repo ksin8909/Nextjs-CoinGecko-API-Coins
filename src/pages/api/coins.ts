b870e3def78435f3cbc8e8dda60e94026f0ffc89
unknown <ksin8909@gmail.com>
2024-05-25 14:41:22 +0800

    2024-05-25

---
+++ src/pages/api/coins.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { promises as fs } from 'fs';
+
+type ResponseData = {
+    message: string
+}
+ 
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseData>
+) {
+    const file = await fs.readFile(process.cwd() + '/src/app/json/coins.json', 'utf8');
+    const data = JSON.parse(file);
+
+    let str = "";
+    for (let i = 0; i < data.length; i++){
+        str += data[i]['coingeckoid'] + "%2C";
+    }
+
+    // price
+    let price_url = "https://api.coingecko.com/api/v3/simple/price?ids=" + str + "&vs_currencies=usd";
+    //const request = require('request');
+    //let options = {json: true};
+
+    const ress = await fetch(price_url);
+    const latestprice = await ress.json();
+
+    res.status(200).json(latestprice)
+}
